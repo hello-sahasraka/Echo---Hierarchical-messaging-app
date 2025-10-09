0ec595dd374f1e7251400080587b414806d9daec
hello-sahasraka <sahasrakasachin@gmail.com>
2025-10-09 17:00:26 +0530

    feat: defined relation of the database tables

--- echo-backend/src/models/sequelize.js
+++ echo-backend/src/models/sequelize.js
@@ -23,4 +23,15 @@ db.messages = Message(sequelize, Sequelize);
 db.chats = Chat(sequelize, Sequelize);
 db.participants = Participant(sequelize, Sequelize);
 
+// Define associations
+// Chat → Messages
+db.chats.hasMany(db.messages, { foreignKey: "chat_id", as: "messages" });
+db.messages.belongsTo(db.chats, { foreignKey: "chat_id", as: "chat" });
+
+db.chats.belongsTo(db.users, { foreignKey: "creator_id", as: "creator" });
+db.users.hasMany(db.chats, { foreignKey: "creator_id", as: "createdChats" });
+
+db.messages.belongsTo(db.users, { foreignKey: "sender_id", as: "sender" });
+db.users.hasMany(db.messages, { foreignKey: "sender_id", as: "sentMessages" });
+
 export default db;
